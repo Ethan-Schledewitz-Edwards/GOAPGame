2fcb4d477d2a6565b98fcbe72d994a290aaa4546
Ethan-Schledewitz-Edwards <144922288+Ethan-Schledewitz-Edwards@users.noreply.github.com>
2026-03-22 20:27:28 -0400

    Consolidate harvestable AIOs

--- Assets/Interactables/Actor_Interactables/Harvestables/HarvestableAIO.cs
+++ Assets/Interactables/Actor_Interactables/Harvestables/HarvestableAIO.cs
@@ -3,19 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(HarvestableHealth))]
-public class StoneAIO : ActorInteractableObjectBase
+public class HarvestableAIO : ActorInteractableObjectBase
 {
-    public override void Interact(Actor actor)
-    {
-        base.Interact(actor);
-    }
 
-    public override void StopInteract()
-    {
-        base.StopInteract();
-    }
+	public override void Interact(Actor actor)
+	{
+		base.Interact(actor);
+	}
 
-    public override void UpdateSpeed(int extra){}
+	public override void StopInteract()
+	{
+		base.StopInteract();
+	}
+
+	public override void UpdateSpeed(int extra) { }
 
 	public override BehaviourTree GetBehaviourTree(Transform userTransform, Actor userActorComp)
 	{
@@ -33,6 +34,7 @@ public class StoneAIO : ActorInteractableObjectBase
 		root.SetData("target", transform);
 
 		tree.SetTree(root);
+
 		return tree;
 	}
 }
